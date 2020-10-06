bdfa57da28293558ae31df4058ec975c9ebd563d
Stefan Lörwald <stefan.loerwald@excubo-ag.com>
2020-10-06 13:26:07 +0200

    added test app

---
+++ IntegrationTest/Component3.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTest
+{
+    public partial class Component3 : ComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(0, "div");
+            builder.CloseElement();
+        }
+        [Parameter] public object P1 { get; set; }
+    }
+}
