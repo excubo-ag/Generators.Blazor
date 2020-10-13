d5472913798f85ec47793eb1a9df1269973d11a9
Stefan Lörwald <stefan.loerwald@excubo-ag.com>
2020-10-13 13:47:34 +0200

    move class

--- Blazor/SetParametersAsyncGenerator.cs
+++ Blazor/SetParametersAsyncGenerator.cs
@@ -192,19 +192,4 @@ namespace {namespaceName}
             }
         }
     }
-
-    public static class TypeSymbolExtension
-    {
-        public static IEnumerable<INamedTypeSymbol> GetTypeHierarchy(this INamedTypeSymbol symbol)
-        {
-            yield return symbol;
-            if (symbol.BaseType != null)
-            {
-                foreach (var type in GetTypeHierarchy(symbol.BaseType))
-                {
-                    yield return type;
-                }
-            }
-        }
-    }
 }
---
+++ Blazor/TypeSymbolExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Collections.Generic;
+
+namespace Excubo.Generators.Blazor
+{
+    public static class TypeSymbolExtension
+    {
+        public static IEnumerable<INamedTypeSymbol> GetTypeHierarchy(this INamedTypeSymbol symbol)
+        {
+            yield return symbol;
+            if (symbol.BaseType != null)
+            {
+                foreach (var type in GetTypeHierarchy(symbol.BaseType))
+                {
+                    yield return type;
+                }
+            }
+        }
+    }
+}
