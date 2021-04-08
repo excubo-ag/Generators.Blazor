fc934769bed50e7da0ac4895f7d0a3d28e6ef875
Stefan Lörwald <10850250+stefanloerwald@users.noreply.github.com>
2021-04-08 10:49:45 +0200

    added test case

---
+++ Tests_Blazor/LambdaEventCallbackAnalyzer/Bind.cs
@@ -0,0 +1,32 @@
+﻿using Tests_Blazor.Helpers;
+using Xunit;
+
+namespace Tests_Blazor
+{
+    public partial class LambdaEventCallbackAnalyzerTests
+    {
+        [Fact]
+        public void Bind()
+        {
+            var userSource = @"
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+namespace Foo
+{
+    public class Bar
+    {
+        public void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenComponent<Bar>(0);
+            builder.AddAttribute(1, ""ValueChanged"", Microsoft.AspNetCore.Components.CompilerServices.RuntimeHelpers.TypeCheck<Microsoft.AspNetCore.Components.EventCallback<System.Int32>>(Microsoft.AspNetCore.Components.EventCallback.Factory.Create<System.Int32>(this, Microsoft.AspNetCore.Components.CompilerServices.RuntimeHelpers.CreateInferredEventCallback(this, __value => value = __value, value))));
+            builder.CloseComponent();
+        }
+        [Parameter] public object Value { get; set; }
+        private int value;
+    }
+}";
+            RunGenerator(userSource, out var generatorDiagnostics, out _);
+            generatorDiagnostics.Verify();
+        }
+    }
+}
